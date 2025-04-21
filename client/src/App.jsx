c6881bf73e10eebc15a649f319cffa76c4cd888d
Rakshitasinghh <rakshita.singhhh@gmail.com>
2025-04-21 13:39:23 +0530

    finalllllly

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
-import LandingPage from './pages/LandingPage';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage'
+import Complaint from './pages/Complaint'
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/complaint" element={<Complaint />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
