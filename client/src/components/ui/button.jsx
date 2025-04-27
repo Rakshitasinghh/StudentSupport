9ff584715ede15e7c85188a1fa5b60bfc3b1c35b
Rakshitasinghh <rakshita.singhhh@gmail.com>
2025-04-27 21:24:18 +0530

    about complaint and landing page done

--- client/src/components/ui/button.jsx
+++ client/src/components/ui/button.jsx
@@ -17,15 +17,30 @@
 
 
 // Button.jsx
-import React from 'react';
+// import React from 'react';
+
+// const Button = ({ children, className = '', onClick, type = 'button', ...rest }) => {
+//   return (
+//     <button
+//       className={`bg-[#E88025] hover:bg-[#E88025] text-white px-6 py-2 rounded-full ${className}`}
+//       onClick={onClick}
+//       className={`!bg-[#FF7B08] !text-white !border-[#FF7B08] hover:!bg-[#e25c0f] font-semibold px-6 py-2 border ${className || 'rounded-full'}`}
+
+//       {...rest}
+//     >
+//       {children}
+//     </button>
+//   );
+// };
+
+// export default Button;
 
 const Button = ({ children, className = '', onClick, type = 'button', ...rest }) => {
   return (
     <button
-      className={`bg-[#E88025] hover:bg-[#E88025] text-white px-6 py-2 rounded-full ${className}`}
       onClick={onClick}
-      className={`!bg-[#FF7B08] !text-white !border-[#FF7B08] hover:!bg-[#e25c0f] font-semibold px-6 py-2 border ${className || 'rounded-full'}`}
-
+      type={type}
+      className={`!bg-[#FF7B08] !text-white !border-[#FF7B08] hover:!bg-[#e25c0f] font-semibold px-6 py-2 border rounded-full ${className}`}
       {...rest}
     >
       {children}
